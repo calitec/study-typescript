c7772bc4fd25291a1c37f318af3263d2eb4737d0
seoyeonggyu <syg0713@gmail.com>
2020-10-20 18:15:20 +0900

    리덕스 썽크 리팩토링 완료

--- refactoring/src/App.tsx
+++ refactoring/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import CounterContainer from './containers/CounterContainer';
-import TodoApp from './containers/TodoApp';
+// import CounterContainer from './containers/CounterContainer';
+// import TodoApp from './containers/TodoApp';
 import GithubProfileLoader from './containers/GithubProfileLoader';
 
 function App() {
--- refactoring/src/modules/github/thunks.ts
+++ refactoring/src/modules/github/thunks.ts
@@ -1,19 +1,21 @@
-import { Dispatch } from 'redux';
-import { ThunkAction } from 'redux-thunk';
-import { RootState } from '..';
-import { GithubAction } from './types';
 import { getUserProfileAsync } from './actions';
 import { getUserProfile } from '../../api/github';
+import createAsyncThunk from '../../lib/createAsyncThunk';
 
-export function getUserProfileThunk( username: string ) {
-  return async ( dispatch: Dispatch ) => {
-    const { request, success, failure } = getUserProfileAsync;
-    dispatch(request());
-    try {
-      const userProfile = await getUserProfile( username );
-      dispatch( success (userProfile) );
-    } catch (e) {
-      dispatch(failure(e));
-    }
-  };
-}
+// export function getUserProfileThunk( username: string ) {
+
+//   return async ( dispatch: Dispatch ) => {
+//     const { request, success, failure } = getUserProfileAsync;
+//     dispatch(request());
+//     try {
+//       const userProfile = await getUserProfile( username );
+//       dispatch( success (userProfile) );
+//     } catch (e) {
+//       dispatch(failure(e));
+//     }
+//   };
+  
+// }
+
+
+export const getUserProfileThunk = createAsyncThunk( getUserProfileAsync, getUserProfile )
